2b21293442682d0cb7681d3bf9e7203a0adaebc3
n1215 <knakae1215@gmail.com>
2020-01-11 20:44:31 +0900

    remove zend-diactoros and use nyholm/psr7

--- worker.php
+++ worker.php
@@ -5,14 +5,16 @@ ini_set('display_errors', 'stderr');
 include 'vendor/autoload.php';
 
 $relay = new Spiral\Goridge\StreamRelay(STDIN, STDOUT);
-$psr7 = new Spiral\RoadRunner\PSR7Client(new Spiral\RoadRunner\Worker($relay));
+$worker = new Spiral\RoadRunner\Worker($relay);
+$psr17Factory = new Nyholm\Psr7\Factory\Psr17Factory();
+$psr7 = new Spiral\RoadRunner\PSR7Client($worker, $psr17Factory, $psr17Factory, $psr17Factory);
 
 while ($req = $psr7->acceptRequest()) {
     try {
-        $resp = new \Zend\Diactoros\Response();
+        $resp = $psr17Factory->createResponse();
         $resp->getBody()->write('Hello world from RoadRunner!');
         $psr7->respond($resp);
-    } catch (\Throwable $e) {
+    } catch (Throwable $e) {
         $psr7->getWorker()->error((string)$e);
     }
 }
